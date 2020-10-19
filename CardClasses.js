0d9e1878822d7036cbfde3d11bccf488c3b0d781
JoshsHiddenTrove <tacofuntrain@gmail.com>
2020-10-19 07:20:38 -0600

    created classes for Cards and spells as a subset of cards

--- CardClasses.js
+++ CardClasses.js
@@ -10,3 +10,51 @@ mtg.set.find('AER')
     console.log(result.set.name) // "Aether Revolt"
 })
  //test
+ class Cards {
+   constructor(name,,color,manacost,rarity,flavor,imageUrl) {
+     this.name = name;
+     this.color = color;
+     this.manacost = manacost;
+     this.rarity = rarity;
+     this.flavor = flavor;
+     this.imageUrl = imageUrl;
+   }
+  get name() {
+    return this.name;
+  }
+  get color() {
+    return this.color;
+  }
+  get manacost() {
+    return this.manacost;
+  }
+  get rarity() {
+    return this.rarity;
+  }
+  get flavor() {
+    return this.flavor;
+  }
+  get imageUrl() {
+    return this.imageUrl;
+  }
+ }
+
+ class Spells extends Cards {
+   constructor() {
+
+   }
+
+ }
+
+ class Creatures extends Cards {
+   constructor(power,toughness) {
+     this.power = power;
+     this.toughness = toughness;
+   }
+   get power() {
+     return this.power;
+   }
+   get toughness() {
+     return this.toughness;
+   }
+ }
