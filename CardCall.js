b242a77a317219c428e5390af853b730acb5f26d
JoshsHiddenTrove <tacofuntrain@gmail.com>
2020-11-16 21:30:05 -0700

    implimented scrolling and qpi query

--- CardCall.js
+++ CardCall.js
@@ -1,26 +1,29 @@
+var express = require('express');
+var app = express();
 const mtg = require('mtgsdk')
 
-mtg.card.find(3)
-.then(result => {
-    console.log(result.card.name == "Black Lotus")
-    // console.log(result.card.colors == "[] ")
-    console.log(result.card.colors)
-    console.log(result.card.manaCost == "{0}")
-    console.log(result.card.rarity == "Rare")
-    console.log(result.card.imageUrl == "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=3&type=card")
+// mtg.card.find(3)
+// .then(result => {
+//     console.log(result.card.name == "Black Lotus")
+//     // console.log(result.card.colors == "[] ")
+//     console.log(result.card.colors)
+//     console.log(result.card.manaCost == "{0}")
+//     console.log(result.card.rarity == "Rare")
+//     console.log(result.card.imageUrl == "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=3&type=card")
+//
+// })
+//
+// mtg.card.where({ name:"squee",color:"Red",manaCost : "", rarity:"" })
+// .then(cards => {
+//   for (i = 0; i < cards.length; i++) {
+//     console.log(cards[i].name)
+//     console.log(cards[i].colors)
+//     console.log(cards[i].manaCost)
+//     console.log(cards[i].rarity)
+//     console.log(cards[i].imageUrl)
+//   }
+// })
 
-})
-
-mtg.card.where({ name:"squee",color:"Red",manaCost : "", rarity:"Rare" })
-.then(cards => {
-  for (i = 0; i < cards.length; i++) {
-    console.log(cards[i].name)
-    console.log(cards[i].colors)
-    console.log(cards[i].manaCost)
-    console.log(cards[i].rarity)
-    console.log(cards[i].imageUrl)
-  }
-})
 //
 // mtg.card.where({ colors: 'Red', name: 'Shaman' })
 // .then(cards => {
@@ -30,3 +33,44 @@ mtg.card.where({ name:"squee",color:"Red",manaCost : "", rarity:"Rare" })
 //   console.log(cards[0].rarity)
 //   console.log(cards[0].imageUrl)
 // })
+
+// app.get('/get_proxy', function(req, res) {
+//   res.setHeader('Content-Type', 'application/json');
+//   if(proxy_list.length == 0) {
+//     update_proxies(function() {
+//       var random = parseInt(Math.random() * proxy_list.length, 10);
+//       res.json({status: 'ok', data: { proxy: proxy_list[random] } });
+//     });
+//   }else{
+//     var random = parseInt(Math.random() * proxy_list.length, 10);
+//     res.json({status: 'ok', data: { proxy: proxy_list[random] } });
+//   }
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/index.html', function(req, res, next) {
+    res.sendFile(__dirname + '/index.html');
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+});
+
+app.get('/getCards', function(req, res, next) {
+  console.log(req.query);
+  mtg.card.where(req.query)
+  .then(cards => {
+    for (i = 0; i < cards.length; i++) {
+      // console.log(cards[i].name);
+      // console.log(cards[i].colors);
+      // console.log(cards[i].manaCost);
+      // console.log(cards[i].rarity);
+      console.log(cards[i].imageUrl);
+    }
+    res.send(cards);
+    console.log("after res.send in getCards");
+  })
+});
+
+// Start the server on port 3000
+app.listen(8080, '127.0.0.1');
+console.log('Node server running on port 8080');
--- Public/test.js
+++ Public/test.js
@@ -1,28 +1,97 @@
+// const mtg = require('mtgsdk')
+
+//document.getElementById("cardDisplay").addEventListener("scroll",populateCards(),false);
+
 function SearchClick() {
-  var CardDiv = document.getElementById("cardDisplay");
-  CardDiv.style.visibility = 'visible';
   addStuff();
-  // document.body.appendChild(btn);
 }
 
-  function addStuff() {
-    var words = ['one', 'two', 'three'];
 
-    var html = '<table>';
+function addStuff() {
+  var html = '';
+  data = { name:"squee",color:"Red",manaCost : "", rarity:"Rare" };
+  data = getSearchValues()
+  cardCall(data);
+  for (i = 0; i < 5; i++) {
+    html += '<div class="card" style="width: 13rem;">';
+    html += '<img class="card-img-top" src="images/testCard.jpg" alt="Card image cap">';
+    html += '</div>';
+  }
+  $("#cardDisplay").append(html);
+}
 
-    $.each(words, function(i, word) {
-      html += '<td> <IMG SRC="images/testCard.jpg" /></td>';
-      html += '<td> <IMG SRC="images/testCard.jpg" /></td>';
-      html += '<td> <IMG SRC="images/testCard.jpg" /></td>';
-      html += '<td> <IMG SRC="images/testCard.jpg" /></td>';
-      html += '<td> <IMG SRC="images/testCard.jpg" /></td>';
-      html += '<td> <IMG SRC="images/testCard.jpg" /></td>';
-      html += '<td> <IMG SRC="images/testCard.jpg" /></td>';
-      html += '</tr>';
+function getSearchValues(){
+  var givenName = document.getElementById("SearchBar").value;
+  var givenRarity = document.getElementById("rarity").value;
+  var givenMana = getMana();
+  var givenManacost = parseInt(document.getElementById("Manacost").value);
+  if(givenManacost > 16){
+    givenManacost = 16;
+  }
+  var givenPower = parseInt(document.getElementById("power").value);
+  if(givenPower > 15){
+    givenPower = 15;
+  }
+  var givenToughness = parseInt(document.getElementById("toughness").value);
+  if(givenToughness > 15){
+    givenToughness = 15;
+  }
+  var searchVals = {name:givenName,color: givenMana, manacost:givenManacost,power:givenPower,toughness:givenToughness,rarity:givenRarity};
+  console.log(searchVals);
+  return searchVals;
+}
 
+function getMana(){
+  var colors = [];
+  if($("#Black").is(":checked")){
+    colors.push("Black");
+  }
+  if($("#blue").is(":checked")){
+    colors.push("Blue");
+  }
+  if($("#green").is(":checked")){
+    colors.push("Green");
+  }
+  if($("#red").is(":checked")){
+    colors.push("Red");
+  }
+  if($("#white").is(":checked")){
+    colors.push("White");
+  }
+  return colors;
+
+}
+
+window.onscroll = function(event){
+  console.log("inside scroll function");
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+    addStuff();
+  }
+};
+
+  function cardCall(CardData){
+    $(document).ready(function() {
+        $.ajax({
+            url: 'http://localhost:8080/getCards',
+            dataType: "json",
+            data: {"data": CardData},
+            type: 'GET',
+            headers: {'Access-Control-Allow-Origin': '*'},
+            success: function (data) { // on success take in urls and append to div
+              var html ='';
+              for (i = 0; i < 5; i++) {
+                html += '<div class="card" style="width: 13rem;">';
+                html += '<img class="card-img-top" src="' + data[i].imageUrl  + '" alt="Card image cap">';
+                html += '</div>';
+              }
+              $("#cardDisplay").append(html);
+              console.log(data);
+              var holder = data;
+            },
+            error: function (xhr, status, error) {
+                console.log('Error: ' + xhr + "\n" + status + "\n" + error);
+                $('#lblResponse').html('Error connecting to the server.');
+            },
+        });
     });
-
-    html += '</table>';
-
-    $("#cardDisplay").append(html);
   }
