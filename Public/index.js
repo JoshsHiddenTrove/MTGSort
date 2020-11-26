4c50822dc04cbb0c476fe0fd03cb31fdc1732fd6
JoshsHiddenTrove <tacofuntrain@gmail.com>
2020-11-26 12:15:10 -0700

    revamped and paginated api calls

--- Public/index.js
+++ Public/index.js
@@ -1,111 +1,135 @@
-function SearchClick() {
-  $("#cardDisplay").html("");
-  addStuff();
-}
-
-
-function addStuff() {
-  var data = getSearchValues();
-  cardCall(data);
-}
-
-function getSearchValues(){
-  var urlAddOn = "?";
-  if(document.getElementById("SearchBar").value != ""){
-    urlAddOn += "name=" + document.getElementById("SearchBar").value +"&";
-  };
-  if(document.getElementById("rarity").value != ""){
-    urlAddOn += "rarity=" + document.getElementById("rarity").value +"&";
+class PageHandler{
+  pages=null;
+  constructor(){
+    this.pages=1;
   }
-  var urlAddOn = getColor(urlAddOn);
-  if(document.getElementById("Type").value != ""){
-    urlAddOn += "type=" + document.getElementById("Type").value +"&";
-  };
-  if(!isNaN(parseInt(document.getElementById("power").value))){
-    var givenPower = parseInt(document.getElementById("power").value);
-    if(givenPower > 15){
-      givenPower = 15;
+
+  searchClick() {
+    $("#cardDisplay").html("");
+    this.pages = 1;
+    this.addStuff();
+  }
+
+  addStuff() {
+    var data = this.getSearchValues();
+    this.cardCall(data);
+  }
+
+  getSearchValues(){
+    var urlAddOn = "?";
+    if(document.getElementById("SearchBar").value != ""){
+      urlAddOn += "name=" + document.getElementById("SearchBar").value +"&";
+    };
+    if(document.getElementById("rarity").value != ""){
+      urlAddOn += "rarity=" + document.getElementById("rarity").value +"&";
     }
-    if(givenPower < 0){
-      givenPower = 0;
+    var urlAddOn = this.getColor(urlAddOn);
+    if(document.getElementById("Type").value != ""){
+      urlAddOn += "type=" + document.getElementById("Type").value +"&";
+    };
+    if(!isNaN(parseInt(document.getElementById("power").value))){
+      var givenPower = parseInt(document.getElementById("power").value);
+      if(givenPower > 15){
+        givenPower = 15;
+      }
+      if(givenPower < 0){
+        givenPower = 0;
+      }
+      urlAddOn += "power=" + givenPower +"&";
     }
-    urlAddOn += "power=" + givenPower +"&";
-  }
-  if(!isNaN(parseInt(document.getElementById("toughness").value))){
-    var givenToughness = parseInt(document.getElementById("toughness").value);
-    if(givenToughness > 15){
-      givenToughness = 15;
+    if(!isNaN(parseInt(document.getElementById("toughness").value))){
+      var givenToughness = parseInt(document.getElementById("toughness").value);
+      if(givenToughness > 15){
+        givenToughness = 15;
+      }
+      if(givenToughness < 0){
+        givenToughness = 0;
+      }
+      urlAddOn += "toughness=" + givenToughness +"&";
     }
-    if(givenToughness < 0){
-      givenToughness = 0;
+    urlAddOn = urlAddOn.slice(0, -1);
+    if(urlAddOn == ""){
+      urlAddOn += "?page=" + parseInt(this.pages);
+      this.pages++;
+    }else{
+      urlAddOn += "&page=" + parseInt(this.pages);
+      this.pages++;
     }
-    urlAddOn += "toughness=" + givenToughness +"&";
+    return urlAddOn;
   }
-  urlAddOn = urlAddOn.slice(0, -1);
-  // if(urlAddOn == ""){
-  //   urlAddOn = "?name=a";
-  // }
-  return urlAddOn;
-}
 
-function getColor(urlAddOn){
-  var colors = "";
-    if($("#Black").is(":checked")){
-    colors += "black,"
-  }
-  if($("#blue").is(":checked")){
-    colors += "blue,"
-  }
-  if($("#green").is(":checked")){
-    colors += "green,"
-  }
-  if($("#red").is(":checked")){
-    colors += "red,"
-  }
-  if($("#white").is(":checked")){
-    colors += "white,"
-  }
-  if(colors != ""){
-    colors = colors.slice(0, -1);
-    urlAddOn += "colors=" + colors +"&";
-  }
-  return urlAddOn;
+  getColor(urlAddOn){
+    var colors = "";
+      if($("#Black").is(":checked")){
+      colors += "black,"
+    }
+    if($("#blue").is(":checked")){
+      colors += "blue,"
+    }
+    if($("#green").is(":checked")){
+      colors += "green,"
+    }
+    if($("#red").is(":checked")){
+      colors += "red,"
+    }
+    if($("#white").is(":checked")){
+      colors += "white,"
+    }
+    if(colors != ""){
+      colors = colors.slice(0, -1);
+      urlAddOn += "colors=" + colors +"&";
+    }
+    return urlAddOn;
 
-}
-
-window.onscroll = function(event){
-  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-    addStuff();
   }
-};
 
-  function cardCall(CardData){
-    var url="https://api.magicthegathering.io/v1/cards" + CardData;
-    // console.log(url);
-    $(document).ready(function() {
-      $.ajax(
-           {
-              type: 'GET',
-              url: url,
-              async: false,
-              success: function(data)
-              {
-                console.log(data.cards);
-              var html ='';
-              for(i=0;i<data.cards.length;i++){
-                if(data.cards[i].imageUrl != undefined){
-                  html += '<div class="card" style="width: 13rem;">';
-                  html += '<img class="card-img-top" src="' + data.cards[i].imageUrl  + '" alt="Card image cap">';
-                  html += '</div>';
+    cardCall(cardData){
+      var url="https://api.magicthegathering.io/v1/cards" + cardData;
+      // console.log(url);
+      $(document).ready(function() {
+        $.ajax(
+             {
+                type: 'GET',
+                url: url,
+                async: false,
+                success: function(data)
+                {
+                  // console.log(data.cards);
+                var html ='';
+                if(data.cards.length != 0){
+                  for(let i=0;i<data.cards.length;i++){
+                    if(data.cards[i].imageUrl != undefined){
+                      html += '<div class="card" style="width: 13rem;">';
+                      html += '<img class="card-img-top" src="' + data.cards[i].imageUrl  + '" alt="Card image cap">';
+                      html += '</div>';
+                    }
+                  }
+                }else{
+                    html+= '<div class="container">';
+                    html+= '<div class="row justify-content-center">';
+                    html+='<h1>No more cards sorry :/</h1>';
+                    html+='</div>';
+                    html+='</div>';
                 }
-              }
-              $("#cardDisplay").append(html);
+                $("#cardDisplay").append(html);
 
-            },
-            error: function (xhr, status, error) {
-                console.log('Error: ' + xhr + "\n" + status + "\n" + error);
-                $('#lblResponse').html('Error connecting to the server.');
-            },
-        });
-    });
-  }
+              },
+              error: function (xhr, status, error) {
+                  console.log('Error: ' + xhr + "\n" + status + "\n" + error);
+                  $('#lblResponse').html('Error connecting to the server.');
+              },
+          });
+      });
+    }
+}
+
+
+function initPageHandler(){
+  var temp = new PageHandler();
+  temp.searchClick();
+  window.onscroll = function(event){
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      temp.addStuff();
+    }
+  };
+}
--- app.js
+++ app.js
@@ -2,48 +2,6 @@ var express = require('express');
 var app = express();
 const mtg = require('mtgsdk')
 
-// mtg.card.find(3)
-// .then(result => {
-//     console.log(result.card.name == "Black Lotus")
-//     // console.log(result.card.colors == "[] ")
-//     console.log(result.card.colors)
-//     console.log(result.card.manaCost == "{0}")
-//     console.log(result.card.rarity == "Rare")
-//     console.log(result.card.imageUrl == "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=3&type=card")
-//
-// })
-//
-// mtg.card.where({ name:"aj", color:"", manaCost : "NAN", rarity:"" })
-// .then(cards => {
-//   for (i = 0; i < cards.length; i++) {
-//     console.log(cards[i].name)
-//
-//   }
-// })
-
-//
-// mtg.card.where({ colors: 'Red', name: 'Shaman' })
-// .then(cards => {
-//   console.log(cards[0].name)
-//   console.log(cards[0].colors)
-//   console.log(cards[0].manaCost)
-//   console.log(cards[0].rarity)
-//   console.log(cards[0].imageUrl)
-// })
-
-// app.get('/get_proxy', function(req, res) {
-//   res.setHeader('Content-Type', 'application/json');
-//   if(proxy_list.length == 0) {
-//     update_proxies(function() {
-//       var random = parseInt(Math.random() * proxy_list.length, 10);
-//       res.json({status: 'ok', data: { proxy: proxy_list[random] } });
-//     });
-//   }else{
-//     var random = parseInt(Math.random() * proxy_list.length, 10);
-//     res.json({status: 'ok', data: { proxy: proxy_list[random] } });
-//   }
-// });
-
 app.use(express.static(__dirname + '/public'));
 
 app.get('/index.html', function(req, res, next) {
